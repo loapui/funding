2571a0be4927d9b09f0c3daea6dda4814d796471
Loapui <kenny-k@foxmail.com>
2016-12-25 21:09:14 +0800

    v0 -> 0.02306

---
+++ src/featureEngineer/base.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 25, 2016
+
+@author: loapui
+'''
+
+from os.path import join, dirname
+
+train_path = join(dirname(dirname(dirname(__file__))), 'train')
+test_path = join(dirname(dirname(dirname(__file__))), 'test')
+temp_path = join(dirname(dirname(dirname(__file__))), 'temp')
+feature_path = join(dirname(dirname(dirname(__file__))), 'features')
+result_path = join(dirname(dirname(dirname(__file__))), 'result')
+
